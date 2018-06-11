d53570a30126e5f84f856d541b4bc10b18e1942f
Vivek Rai <vijayjaidewan01@gmail.com>
2018-06-11 15:00:49 +0530

    Nav Drawer added

--- app/src/main/java/com/vijayjaidewan01vivekrai/collapsingtoolbar_github/ScrollingActivity.java
+++ app/src/main/java/com/vijayjaidewan01vivekrai/collapsingtoolbar_github/ScrollingActivity.java
@@ -1,9 +1,16 @@
 package com.vijayjaidewan01vivekrai.collapsingtoolbar_github;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.NestedScrollView;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,8 +28,10 @@ import com.vijayjaidewan01vivekrai.collapsingtoolbar_github.Adapters.CardAdapter
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class ScrollingActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
     private Toolbar mToolbar;
     private Toolbar toolbar;
     private CoordinatorLayout coordinatorLayout;
@@ -31,6 +40,8 @@ public class ScrollingActivity extends AppCompatActivity {
     private LinearLayout linearLayout;
     private RecyclerView recyclerView;
     private NestedScrollView nestedScrollView;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private SwipeRefreshLayout swipeRefreshLayoutCoordinator;
 
     int collapseValue = 2;
     int searchValue = 0;
@@ -46,6 +57,8 @@ public class ScrollingActivity extends AppCompatActivity {
         appBarLayout = findViewById(R.id.app_bar);
         coordinatorLayout = findViewById(R.id.coordinator_layout);
         nestedScrollView = findViewById(R.id.scrollView);
+        swipeRefreshLayout = findViewById(R.id.swipe);
+        swipeRefreshLayoutCoordinator = findViewById(R.id.swipe_coordinator);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar = findViewById(R.id.tool_bar);
@@ -58,11 +71,29 @@ public class ScrollingActivity extends AppCompatActivity {
         if (collapseValue == 1) {
             recyclerView = findViewById(R.id.recyclerViewLinear);
 
+
             setSupportActionBar(mToolbar);
             getSupportActionBar().setTitle(R.string.title);
             coordinatorLayout.setVisibility(View.GONE);
             //linearLayout.setVisibility(View.VISIBLE);
             //layout.setVisibility(View.VISIBLE);
+
+
+            swipeRefreshLayout.setProgressBackgroundColorSchemeColor(Color.WHITE);
+            swipeRefreshLayout.setColorSchemeColors(Color.MAGENTA, Color.YELLOW,Color.GREEN, Color.RED, Color.BLUE);
+            swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            swipeRefreshLayout.setRefreshing(false);
+
+                        }
+                    },3000);
+
+                }
+            });
         }
         if (collapseValue == 2) {
             recyclerView = findViewById(R.id.recycler_view);
@@ -74,6 +105,21 @@ public class ScrollingActivity extends AppCompatActivity {
             mToolbar.setVisibility(View.GONE);
 
             setRecyclerViewMargins();
+            swipeRefreshLayoutCoordinator.setProgressBackgroundColorSchemeColor(Color.WHITE);
+            swipeRefreshLayoutCoordinator.setColorSchemeColors(Color.MAGENTA, Color.YELLOW,Color.GREEN, Color.RED, Color.BLUE);
+            swipeRefreshLayoutCoordinator.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+                @Override
+                public void onRefresh() {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            swipeRefreshLayoutCoordinator.setRefreshing(false);
+
+                        }
+                    },3000);
+
+                }
+            });
             //layout.setVisibility(View.GONE);
         }
 
@@ -91,6 +137,7 @@ public class ScrollingActivity extends AppCompatActivity {
         setAdapter(3);
 
 
+
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,6 +146,15 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        if (collapseValue == 2){
+
+            drawerLayout = findViewById(R.id.drawer_layout);
+            toggle = new ActionBarDrawerToggle(ScrollingActivity.this,drawerLayout, R.string.open, R.string.close);
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
     }
 
     public void setAdapter()
@@ -148,12 +204,12 @@ public class ScrollingActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (toggle.onOptionsItemSelected(item)){
             return true;
         }
+        int id = item.getItemId();
+
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -166,18 +222,23 @@ public class ScrollingActivity extends AppCompatActivity {
                 RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
                 if(Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()){
                     //Log.d("appbar",""+recyclerView.getScaleY());
-                    CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) recyclerView.getLayoutParams(); // Redundant Code with line 119
-
+                    CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) swipeRefreshLayoutCoordinator.getLayoutParams(); // Redundant Code with line 119
                     layoutParams.setMargins(0,0,0,0);
-                    recyclerView.setLayoutParams(layoutParams);
+                    swipeRefreshLayoutCoordinator.setLayoutParams(layoutParams);
                     //Animate the scrolling
                 }
                 else if(Math.abs(verticalOffset) == 0){
-                    CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams)recyclerView.getLayoutParams();
+                   CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams)swipeRefreshLayoutCoordinator.getLayoutParams();
                     layoutParams.setMargins(0,150,0,0);
-                    recyclerView.setLayoutParams(layoutParams);
+                    swipeRefreshLayoutCoordinator.setLayoutParams(layoutParams);
+
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
